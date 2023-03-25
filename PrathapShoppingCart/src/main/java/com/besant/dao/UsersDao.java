d8721aa2c758684e3689208215046919bbdc7b97
pradabdhan p <prathapdrascal@gmial.com>
2023-03-25 11:54:42 +0530

    first commit

---
+++ PrathapShoppingCart/src/main/java/com/besant/dao/UsersDao.java
@@ -0,0 +1,46 @@
+package com.besant.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.besanttech.entities.User;
+
+public class UsersDao {
+	static Connection con = null;
+	static {
+		// initialize data source connections
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/myshopppingstore", "root", "root");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public User getUserDetails(User user) {
+
+		try {
+		String loginQuery = "select * from users where user_name =? and user_pwd=?";
+		PreparedStatement stmt = con.prepareStatement(loginQuery);
+		stmt.setString(1, user.getUserName());
+		stmt.setString(2, user.getPassword());
+
+		ResultSet rs = stmt.executeQuery();
+		while (rs.next()) {
+			System.out.print("login details correct");
+			return user;
+		}
+
+	
+	}
+
+	catch(Exception e){
+		System.out.println("Exception: "+e.getMessage());
+	}
+		return null;
+}}
---
+++ PrathapShoppingCart/src/main/webapp/views/login.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+Welcome to shopping cart, please login to continue.
+
+<form action="login" method="post">  
+Name:<input type="text" name="username"/><br/><br/>  
+Password:<input type="password" name="userpass"/><br/><br/>  
+<input type="submit" value="login"/>  
+</form>
+</body>
+</html>
+
